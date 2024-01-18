09598206fd24c8052d585b8a3adbb0335824aa9c
elinfelicia <elinriihimaki@gmail.com>
2024-01-18 08:59:27 +0100

    content editable

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
-import axios from "axios";
+document.addEventListener('DOMContentLoaded', () => {
+  // Date and time display
+  function dateTimeFn() {
+    const now = new Date();
+    const currentDate = now.toDateString();
+    document.querySelector("#currentDate").textContent = currentDate;
+    
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    document.querySelector("#currentTime").textContent = `${hour}:${minute}`;
+  }
+  setInterval(dateTimeFn, 1000);
 
-function dateTimeFn() {
-  const now = new Date();
+  // Custom Greeting
+  const welcomeMsg = document.querySelector("#welcomeMessage");
+  const customGreeting = localStorage.getItem("welcomeMsgContent");
+  if (customGreeting) {
+    welcomeMsg.textContent = customGreeting;
+  }
+  welcomeMsg.addEventListener("input", () => {
+    const content = welcomeMsg.textContent;
+    localStorage.setItem("welcomeMsgContent", content);
+  });
+
+  // Save notes
+  const notepad = document.querySelector("#notesArea");
+  const savedNotes = localStorage.getItem("notepadContent");
+  if (savedNotes) {
+    notepad.textContent = savedNotes;
+  }
+  notepad.addEventListener("input", () => {
+    const noteContent = notepad.value;
+    localStorage.setItem("notepadContent", noteContent);
+  });
   
-  const currentDate = now.toDateString();
-  document.querySelector("#currentDate").textContent = currentDate;
-  
-  let hour = now.getHours()
-  let minute = now.getMinutes()
-  document.querySelector("#currentTime").textContent = `${hour}:${minute}`
-}
-setInterval(dateTimeFn, 1000)
-
-
-
-const welcomeMsg = document.querySelector("#welcomeMessage");
-
-const customGreeting = localStorage.getItem('welcomeMsgontent');
-if (customGreeting) {
-  welcomeMsg.textContent = customGreeting;
-}
-
-// Add an event listener to the h1 element to save its content on change
-welcomeMsg.addEventListener('input', () => {
-  const content = welcomeMsg.textContent;
-  localStorage.setItem('wlecomeMsgContent', content);
 });
